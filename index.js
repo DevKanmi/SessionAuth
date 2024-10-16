209dc3a9baa935a11e85a30fed1b9356774e8143
Akinbayo Clinton <clintexakinbs@gmail.com>
2024-10-16 22:13:49 +0100

    Adjusted DB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import { app } from "./src/app.js";
 
 config()
+
 PORT = process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`server is running on port 3000`);
--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -4,9 +4,7 @@ import { config } from "dotenv";
 config();
 
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 
   mongoose.set('strictQuery', false)
 
